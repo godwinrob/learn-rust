2b5150bca3a40b1f8dd249635d6676099999104a
godwinrob <rob@robgodwin.com>
2023-10-09 11:14:22 -0400

    updatind session 1

---
+++ session_1/hello_cargo/target/debug/deps/hello_cargo-49f7128a8baa57eb.d
@@ -0,0 +1,5 @@
+/Users/robgodwin/Documents/workspace/godwinrob/learn-rust/session_1/hello_cargo/target/debug/deps/hello_cargo-49f7128a8baa57eb: src/main.rs
+
+/Users/robgodwin/Documents/workspace/godwinrob/learn-rust/session_1/hello_cargo/target/debug/deps/hello_cargo-49f7128a8baa57eb.d: src/main.rs
+
+src/main.rs:
---
+++ session_1/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/robgodwin/Documents/workspace/godwinrob/learn-rust/session_1/hello_cargo/target/debug/hello_cargo: /Users/robgodwin/Documents/workspace/godwinrob/learn-rust/session_1/hello_cargo/src/main.rs
